d2468590815f9c00e9a8c2bc93a6780103783fae
BrianCaetano <brian-caetano@hotmail.com>
2021-08-23 17:56:01 -0300

    adiciona DOM do pkmn inimigo

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const randomNumber = Math.floor(Math.random() * 152);
 
@@ -14,15 +14,33 @@ async function fetchPkmn(id) {
     let type2 = type1;
     if (resolve.types[1]) type2 = resolve.types[1].type.name;
 
+    return {
+      name,
+      spriteFront,
+      spriteBack,
+      type1,
+      type2,
+    }
   } catch (error) {
     await console.log(error);
   }
 }
-fetchPkmn(randomNumber);
+// fetchPkmn(randomNumber);
 
-function enemyPkmn() {
+async function enemyPkmn(callback) {
   const enemyName = document.querySelector('#enemy-name');
   const enemySpriteFront = document.querySelector('#enemy-sprite-front');
   const enemyType1 = document.querySelector('#enemy-type1');
   const enemyType2 = document.querySelector('#enemy-type2');
+
+  enemyName.innerHTML = await callback(randomNumber).then(use => use.name);
+  enemySpriteFront.src = await callback(randomNumber).then(use => use.spriteFront);
+  enemyType1.innerHTML = await callback(randomNumber).then(use => use.type1)
+  enemyType2.innerHTML = await callback(randomNumber).then(use => use.type2)
+
 }
+
+window.onload = async () => {
+  await enemyPkmn(fetchPkmn)
+};
+

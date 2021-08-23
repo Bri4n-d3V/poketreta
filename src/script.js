d7e4d8999173bf2d86a04a75e245541c102a10ab
BrianCaetano <brian-caetano@hotmail.com>
2021-08-23 17:01:19 -0300

    adiciona spriteBack na func fetchPkmn

--- src/script.js
+++ src/script.js
@@ -8,7 +8,8 @@ async function fetchPkmn(id) {
     const resolve = await endpoint.json();
 
     const name = resolve.name;
-    const image = resolve.sprites.front_default;
+    const spriteFront = resolve.sprites.front_default;
+    const spriteBack = resolve.sprites.back_default;
     const type1 = resolve.types[0].type.name;
     let type2 = type1;
     if (resolve.types[1]) type2 = resolve.types[1].type.name;

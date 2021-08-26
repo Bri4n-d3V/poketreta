b8772fc668a7f91a14782d7415f81809025aa6d2
MartinSilva <martinempreendedor@gmail.com>
2021-08-25 22:36:03 -0300

    tes ai

--- src/script.js
+++ src/script.js
@@ -358,7 +358,7 @@ function battleCalculator() {
 
     let result = `It's not very effective...`;
     window.alert(result);
-    if (Math.floor(Math.random() * 2) === 0) {
+    if (!(Math.floor(Math.random() * 5) === 0)) {
       points -= 10;
       scoreBoard(points);
       (async (numero) => {
